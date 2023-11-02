36995e74cac6d59fffd295b19e5211f9ebf6714c
Aleksi Jarva <aleksijoonatan@gmail.com>
2023-11-02 14:01:42 +0200

    skeleton for host cli

--- host/src/main.rs
+++ host/src/main.rs
@@ -15,19 +15,58 @@ use std::{io::Read, mem::size_of};
 // Libraries
 use corncobs::{max_encoded_len, ZERO};
 use serial2::SerialPort;
+use std::io;
+use std::io::Write;
 
 // Application dependencies
 use host::open;
-use shared::{deserialize_crc_cobs, serialize_crc_cobs, Ack, Command, OUT_SIZE, IN_SIZE}; // local library
+use shared::{deserialize_crc_cobs, serialize_crc_cobs, Ack, Command, IN_SIZE, OUT_SIZE}; // local library
 
 type InBuf = [u8; IN_SIZE];
 type OutBuf = [u8; OUT_SIZE];
 
 fn main() -> Result<(), std::io::Error> {
     let mut port = open()?;
-
     let mut out_buf = [0u8; OUT_SIZE];
     let mut in_buf = [0u8; IN_SIZE];
+
+    loop {
+        println!("\nTASKS: \n 1. Toggle RGB on \n 2. Toggle RGB off \n 3. Set blink data\n 4. Set date time\n");
+        print!(" > ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+
+        let task: u32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid input");
+                continue;
+            }
+        };
+
+        match task {
+            1 => {
+                let response = request(&Command::RgbOn, &mut port, &mut out_buf, &mut in_buf)?;
+            }
+            2 => {
+                let response = request(&Command::RgbOff, &mut port, &mut out_buf, &mut in_buf)?;
+            }
+            3 => {
+                println!("3");
+            }
+            4 => {
+                println!("4");
+            }
+            _ => {
+                println!("Invalid task selected");
+                continue;
+            }
+        }
+    }
 
     let cmd_off = Command::SetBlinker(shared::BlinkerOptions::Off);
     let cmd_on = Command::SetBlinker(shared::BlinkerOptions::On {
