e6e094589b333d2ccb3b1708546609059da40292
root <root@LAPTOP-8HKOC9H1>
2023-11-02 13:57:55 +0200

    Implemented set_blink_data and update_rgb_state

--- esp32c3/src/main.rs
+++ esp32c3/src/main.rs
@@ -15,18 +15,26 @@ mod app {
         prelude::*,
         uart::{
             config::{Config, DataBits, Parity, StopBits},
-            TxRxPins, UartRx, UartTx
+            TxRxPins, UartRx, UartTx,
         },
         Uart, IO,
     };
 
-    use shared::{Command, BlinkerOptions, Ack,
-    OUT_SIZE, IN_SIZE,
-    deserialize_crc_cobs, serialize_crc_cobs};
+    use shared::{
+        deserialize_crc_cobs, serialize_crc_cobs, Ack, BlinkerOptions, Command, IN_SIZE, OUT_SIZE,
+    };
+
+    #[derive(Debug)]
+    pub enum RgbState {
+        On,
+        Off,
+    }
 
     #[shared]
     struct Shared {
         cmd: [u8; OUT_SIZE],
+        rgb_state: RgbState,
+        blink_data: BlinkerOptions,
     }
 
     #[local]
@@ -73,47 +81,60 @@ mod app {
         let (uart_tx, uart_rx) = uart0.split();
 
         rprintln!("init works");
-        let cmd: [u8; OUT_SIZE] = [0; OUT_SIZE];
-        let cmd_idx: usize = 0;
 
-        (Shared{cmd}, Local{uart_rx, uart_tx, cmd_idx})
+        (
+            Shared {
+                blink_data: BlinkerOptions::Off,
+                cmd: [0; OUT_SIZE],
+                rgb_state: RgbState::Off,
+            },
+            Local {
+                uart_rx,
+                uart_tx,
+                cmd_idx: 0,
+            },
+        )
     }
 
     #[task(binds = UART0, local = [cmd_idx, uart_rx], shared = [cmd])]
     fn aggregate(mut cx: aggregate::Context) {
         rprint!("received UART0 rx interrupt: ");
 
-       if let nb::Result::Ok(c) = cx.local.uart_rx.read() {
-           rprint!("{}", c);
-           cx.shared.cmd.lock(|cmd|{
-               cmd[*cx.local.cmd_idx] = c;
-               *cx.local.cmd_idx += 1;
-           });
+        if let nb::Result::Ok(c) = cx.local.uart_rx.read() {
+            rprint!("{}", c);
+            cx.shared.cmd.lock(|cmd| {
+                cmd[*cx.local.cmd_idx] = c;
+                *cx.local.cmd_idx += 1;
+            });
 
-           if c == 0 {
-               rprint!(" full packet at {}", *cx.local.cmd_idx);
-               broker::spawn().unwrap();
-               *cx.local.cmd_idx = 0;
-           }
-       }
+            if c == 0 {
+                rprint!(" full packet at {}", *cx.local.cmd_idx);
+                broker::spawn().unwrap();
+                *cx.local.cmd_idx = 0;
+            }
+        }
 
-       rprintln!("");
-       cx.local.uart_rx.reset_rx_fifo_full_interrupt();
+        rprintln!("");
+        cx.local.uart_rx.reset_rx_fifo_full_interrupt();
     }
 
     #[task(shared = [cmd], local = [uart_tx])]
     async fn broker(mut cx: broker::Context) {
-        let cmd = cx.shared.cmd.lock(|cmd|{
-            deserialize_crc_cobs::<Command>(cmd)
-        });
+        let cmd = cx
+            .shared
+            .cmd
+            .lock(|cmd| deserialize_crc_cobs::<Command>(cmd));
 
         let ack = if let Ok(cmd) = cmd {
             match cmd {
-                Command::SetBlinker(options) => {set_blink_data::spawn(options).unwrap();}
-                _ => {}
+                Command::SetBlinker(options) => {
+                    set_blink_data::spawn(options).unwrap();
+                }
+                Command::SetDateTime(_) => set_date_time::spawn().unwrap(),
+                Command::RgbOn => update_rgb_data::spawn(RgbState::On).unwrap(),
+                Command::RgbOff => update_rgb_data::spawn(RgbState::Off).unwrap(),
             }
             Ack::Ok
-
         } else {
             cmd.unwrap_err();
             Ack::NotOk
@@ -125,24 +146,28 @@ mod app {
         let _ = cx.local.uart_tx.write_bytes(&buf);
     }
 
-    #[task()]
-    async fn set_blink_data(cx: set_blink_data::Context, options: BlinkerOptions) {
-        rprintln!("set_blink_data");
+    #[task(shared = [blink_data])]
+    async fn set_blink_data(mut cx: set_blink_data::Context, options: BlinkerOptions) {
+        rprintln!("Inside set_blink_data task");
+        cx.shared
+            .blink_data
+            .lock(|blink_data| *blink_data = options)
+    }
+
+    #[task(shared = [rgb_state])]
+    async fn update_rgb_data(mut cx: update_rgb_data::Context, state: RgbState) {
+        rprintln!("Inside update rgb task");
+        cx.shared.rgb_state.lock(|rgb_state| {
+            *rgb_state = state;
+        })
     }
 
     #[task()]
-    async fn set_rgb_data(cx: set_rgb_data::Context) {
-    }
+    async fn set_date_time(cx: set_date_time::Context) {}
 
     #[task()]
-    async fn set_date_time(cx: set_date_time::Context) {
-    }
+    async fn blink(cx: blink::Context) {}
 
     #[task()]
-    async fn blink(cx: blink::Context) {
-    }
-
-    #[task()]
-    async fn update_rgb(cx: update_rgb::Context) {
-    }
+    async fn update_rgb(cx: update_rgb::Context) {}
 }
